a80a4bf5c0386ec92aac29a4b46e955d39de40a4
jamiepoole750 <jamiepoole750@gmail.com>
2025-01-22 20:33:15 -0600

    Exercise 1

---
+++ week1/exercises/ex_1/main.js
@@ -0,0 +1,7 @@
+ //Part 1 Hello World
+ const hello = "Hello, World!";
+ console.log(hello);
+ 
+ //Part 2 What is your name
+ const name = window.prompt("What is your name?");
+ console.log("Hello," + name);
