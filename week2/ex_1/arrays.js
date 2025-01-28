be0df98f0e438bced56fc84c827d9a7d0cf2fdff
jamiepoole750 <jamiepoole750@gmail.com>
2025-01-27 19:36:22 -0600

    wk2_exercise1

---
+++ week2/ex_1/arrays.js
@@ -0,0 +1,13 @@
+// Top 5 favorite movies
+const myArr = ['10 Things I Hate About You', 'Anastasia', 'Narnia', 'About Time', 'We Live in Time'];
+console.log(myArr);
+
+//Prompt to enter movie
+const name = window.prompt("What is your favorite movie?");
+
+//Adding movie to the array using push 
+myArr.push(name);
+
+//display movie list with toString
+console.log(myArr.toString());
+ 
