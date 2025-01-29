62bda61039cdd2c89d674049337201696b350b8b
jamiepoole750 <jamiepoole750@gmail.com>
2025-01-28 20:41:57 -0600

    Added type of game

--- week2/ex_2/objects.js
+++ week2/ex_2/objects.js
@@ -4,14 +4,14 @@ console.log(favGames);
 
 //Objects and properties 
 const games = [
-    { title: ' Rummy Royal ', numberOfPlayers: '2-8 players,', rating: '4/5 stars, ', shortDesciption: ' a deck of cards is split amongst players and players must get rid of their cards to have a chance at collecting chips.' },
-    { title: ' Careers ', numberOfPlayers: '2-6 players,', rating: '5/5 stars, ', shortDesciption: ' you travel around the board and enter different occupations to gain fame, happiness, and fortune.' },
-    { title: ' Speed Scrabble ', numberOfPlayers: '4-6 players,', rating: '4/5 stars, ', shortDesciption: ' tiles are placed face down on table and players grab the tiles to build their own scrabble grid.'},
+    { title: ' Rummy Royal ', type: ' card game', numberOfPlayers: '2-8 players,', rating: '4/5 stars, ', shortDesciption: ' a deck of cards is split amongst players and players must get rid of their cards to have a chance at collecting chips.' },
+    { title: ' Careers ', type: ' board game', numberOfPlayers: '2-6 players,', rating: '5/5 stars, ', shortDesciption: ' you travel around the board and enter different occupations to gain fame, happiness, and fortune.' },
+    { title: ' Speed Scrabble ', type: ' board game', numberOfPlayers: '4-6 players,', rating: '4/5 stars, ', shortDesciption: ' tiles are placed face down on table and players grab the tiles to build their own scrabble grid.'},
 ];
 
 //user prompt
 const promptResponse = window.prompt("I have 3 games in my collection. Pick a number between 1 and 3 and I'll tell you about that game.");
 
 //result of prompt
-console.log ("You selected" + games[ promptResponse -1 ].title + "which is played with " + games[ promptResponse -1 ].numberOfPlayers + " rated " + games[ promptResponse -1].rating + "where" + games[ promptResponse -1].shortDesciption);
+console.log ("You selected" + games[ promptResponse -1 ].title + "which is a" + games[ promptResponse -1].type + ", played with " + games[ promptResponse -1 ].numberOfPlayers + " rated " + games[ promptResponse -1].rating + "where" + games[ promptResponse -1].shortDesciption);
 
