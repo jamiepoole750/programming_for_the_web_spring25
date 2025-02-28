3b8c048647bacd01eefa847f089813255595978d
jamiepoole750 <jamiepoole750@gmail.com>
2025-02-27 21:47:18 -0600

    6.2 Exercise

---
+++ week6/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  const weather = "Showers";
+  const month = "May";
+
+  return (
+      <div>
+        <h1>Hello World</h1>
+        <h2>April {weather}, Bring {month} Flowers!</h2>
+      </div>
+  )
+}
+
+export default App
